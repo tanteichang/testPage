7fd10672d3f550586dacb50cfdc3bff01a675374
changjiaming [常佳明] <changjiaming@xiaomi.com>
2020-02-18 19:02:20 +0800

    v1.0

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+
+
+function initApplication(el) {
+    var dataSource = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        prefetch: './data/datax.json',
+    });
+    var jqueryEl = $(el)
+    jqueryEl.on('typeahead:select', function (unused, obj) {
+        return navigate(obj);
+    });
+    jqueryEl.keyup(function (k) {
+        if (k.keyCode === 13) {
+            var selectables = jqueryEl.siblings(".tt-menu").find(".tt-selectable");
+            $(selectables[0]).trigger("click");
+        }
+    });
+    function navigate(record) {
+        window.location.href = "https://www.npmjs.org/package/@types/" + record.value;
+    }
+    jqueryEl.typeahead(
+        null,
+        {
+            name: 'dependenciesSearch',
+            display: 'value',
+            source: dataSource,
+            templates: {
+                empty: [
+                    '<div class="empty-message">',
+                      '暂未收录此库，请点击此<a href="https://www.google.com">链接</a>进行反馈',
+                    '</div>'
+                  ].join('\n'),
+            },
+        },
+    );
+}
